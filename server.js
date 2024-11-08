3a48ea85f752131af0fb8bb8d29477c4f20b7f3c
Diego Torres <diegotorres0303@gmail.com>
2024-11-08 10:27:57 -0600

    adding server file

---
+++ server.js
@@ -0,0 +1,35 @@
+const { Git } = require("./dist");
+
+// const { join } = require("path");
+const port =
+  !process.env.PORT || isNaN(process.env.PORT)
+    ? 3000
+    : parseInt(process.env.PORT);
+
+// const repos = new Git(join(__dirname, '../repo'), {
+const repos = new Git("./repo", {
+  autoCreate: true,
+});
+
+repos.on("push", (push) => {
+  console.log(`push ${push.repo}/${push.commit} ( ${push.branch} )`);
+  push.accept();
+});
+
+repos.on("pull", (pull) => {
+  console.log(`pull ${pull.repo}/${pull.commit} ( ${pull.branch} )`);
+  pull.accept();
+});
+
+repos.on("fetch", (fetch) => {
+  console.log(`fetch ${fetch.commit}`);
+  fetch.accept();
+});
+
+repos.listen(port, () => {
+  console.log(`node-git-server running at http://localhost:${port}`);
+});
+
+console.log(repos.listen, port)
+
+// repos.handle({ test: true });
